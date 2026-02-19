3844ac658ad7a3a798af770237d3b4f435f5b29d
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-02-19 23:57:41 +0600

    navbar section

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../components/Footer';
 
 const MainLayouts = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <div>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Router/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import MainLayouts from "../LayOuts/MainLayouts";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        Component:MainLayouts,
+        children:[
+            {
+                index:true,
+                Component:Home
+            }
+        ]
+    }
+])
+export default router;
