983d2482e4be53239cfa23aee9fefc2dadd7df14
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-02-23 12:06:22 +0600

    context

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/firebase.init';
+
+const AuthProvider = ({children}) => {
+     const [user,setUser] = useState(null);
+     const [loading,setLoading] = useState(true);
+    const createUser = (email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    const userInfo={
+         user,
+         loading,
+         createUser,
+    }
+    return (
+        <AuthContext value={userInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
