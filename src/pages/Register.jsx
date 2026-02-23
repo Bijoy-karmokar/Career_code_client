9f2b81b2c7c321575ad6d2c5df05f02081980995
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-02-23 11:59:10 +0600

    firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,13 @@ import React from "react";
 import RegisterData from '../assets/Lottie/RegisterLottie.json'
 
 const Register = () => {
+  const handleRegister = (e)=>{
+         e.preventDefault();
+         const form = e.target;
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+        console.log(data);
+  }
   return (
     <div className="hero bg-base-200 min-h-screen ">
       <div className="hero-content gap-20 flex-col lg:flex-row-reverse">
@@ -12,15 +19,15 @@ const Register = () => {
         <div className="card bg-base-100 w-full max-w-2xl flex-1 shrink-0 shadow-2xl">
           <div className="card-body">
             <h1 className="text-5xl font-bold">Register now!</h1>
-            <form className="fieldset">
+            <form onSubmit={handleRegister} className="fieldset">
               <label className="label">Name</label>
-              <input type="text" className="input" placeholder="Enter your name" />
+              <input type="text" name="name" className="input" placeholder="Enter your name" />
               <label className="label">PhotoURL</label>
-              <input type="file" className="input" placeholder="Enter your photoURL" />
+              <input type="file"  name="photoURL" className="input" placeholder="Enter your photoURL" />
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Enter your Email" />
+              <input type="email" name="email" className="input" placeholder="Enter your Email" />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input type="password" name="password" className="input" placeholder="Password" />
               <button className="btn btn-neutral mt-4">Register</button>
             </form>
           </div>
